0049b011daa2a1fc700e1fa550981e812a656582
오하늘 <>
2024-01-11 16:56:16 +0900

    axes helper 추가

--- main.js
+++ main.js
@@ -4,17 +4,28 @@ const canvas = document.querySelector("canvas")
 const scene = new THREE.Scene();
 
 
+
+
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: "red" });
-const mesh = new THREE.Mesh(geometry, material);
+const box = new THREE.Mesh(geometry, material);
 
-scene.add(mesh);
+
+
+scene.add(box);
+
+console.log(box.position.length())
 
 // camera 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 3
 
 scene.add(camera);
+
+// axes helper
+const axesHelper = new THREE.AxesHelper(2); // 2: 선의 길이
+
+scene.add(axesHelper)
 
 
 const renderer = new THREE.WebGLRenderer({ canvas });
