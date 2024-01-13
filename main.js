55cf0886e18c5e3fb224a05941563d1402d08dfb
Sky <nebulass@naver.com>
2024-01-13 17:17:18 +0900

    3d text

--- main.js
+++ main.js
@@ -1,181 +1,92 @@
-import * as THREE from 'three';
-import gsap from 'gsap'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import GUI from 'lil-gui';
-import {RGBELoader} from "three/examples/jsm/loaders/RGBELoader" // 배경 추가
-
-
+import * as THREE from "three";
+import gsap from "gsap";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import GUI from "lil-gui";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader"; // 배경 추가
+import typefaceFont from "three/examples/fonts/helvetiker_regular.typeface.json"; // font import
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
 
 /* GUI (Debug) 설정 */
-const gui = new GUI()
+const gui = new GUI();
 
-const canvas = document.querySelector("canvas")
+const canvas = document.querySelector("canvas");
 const scene = new THREE.Scene();
 
-
-
-/* Texture */
+/* Texture Loader */
 const textureLoader = new THREE.TextureLoader();
+const matcapTextuer = textureLoader.load("/textures/matcaps/1.png");
+console.log(matcapTextuer, "matcapTextuer");
+// matcapTexture.colorSpace = THREE.SRGBColorSpace
 
-const doorColorTexture = textureLoader.load("/textures/door/color.jpg")
-const doorAlphaColorTexture = textureLoader.load("/textures/door/alpha.jpg")
-const doorAmbientOcclusionTexture = textureLoader.load("/textures/door/ambientOcclusion.jpg")
-const doorHeightTexture = textureLoader.load("/textures/door/height.jpg")
-const doorMetalnessTexture = textureLoader.load("/textures/door/metalness.jpg")
-const doorRoughnessTexture = textureLoader.load("/textures/door/roughness.jpg")
-const doorNormalTexture = textureLoader.load("/textures/door/normal.jpg")
-const matcapTexture = textureLoader.load("/textures/matcaps/1.png")
-const gradientTexture = textureLoader.load("/textures/gradients/3.jpg")
+/* Font */
 
-doorColorTexture.colorSpace = THREE.SRGBColorSpace
-matcapTexture.colorSpace = THREE.SRGBColorSpace
+const fontLoader = new FontLoader();
+fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
+  const textGeometry = new TextGeometry("Marshot", {
+    font,
+    size: 0.5,
+    height: 0.2,
+    curveSegments: 5, // 둥근 정도, 숫자가 낮으면 각져보인다
+    bevelEnabled: true, //
 
-/* Object */
+    bevelThickness: 0.03, // 텍스트의 굵기 (z축으로)
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 4, // 둥근 부분에 segements, 숫자가 높으면 더 정교함
+  });
 
-// MeshBasicMaterial
-// const material = new THREE.MeshBasicMaterial({
-//   // map: doorColorTexture,
-//   // color: "green"
-  
-// })
+  /* 처음에 로드되면 텍스트가 중앙에 위치하지 않은데, 중앙에 위치하도록 설정 */
+  // 어려운 방법..
+  // textGeometry.computeBoundingBox();
+  // textGeometry.translate(
+  //   -textGeometry.boundingBox.max.x * 0.5,
+  //   -textGeometry.boundingBox.max.y * 0.5,
+  //   -textGeometry.boundingBox.max.z * 0.5
+  // );
 
-// material.color = "green" <= 작동하지 않음
-// material.color = new THREE.Color("green") <= 클래스로 보내 줘야 함
-// material.wireframe = true
+  textGeometry.center();
 
-// opacity는 alpha를 조절해야 하기 때문에 transparent를 추가해 줘야 함
-// material.transparent = true
-// material.opacity = 0.5
-// material.alphaMap = doorColorTexture
+  const textMaterial = new THREE.MeshMatcapMaterial({
+    matcap: matcapTextuer,
+  });
+  // textMaterial.wireframe = true;
 
-// material.side = THREE.DoubleSide // 오브젝트 앞뒷면 둘다 보이게
+  const text = new THREE.Mesh(textGeometry, textMaterial);
+  scene.add(text);
+  const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
+  const donutMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTextuer });
+  for (let i = 0; i < 100; i++) {
+    const donut = new THREE.Mesh(donutGeometry, donutMaterial);
+    donut.position.x = (Math.random() - 0.5) * 10;
+    donut.position.y = (Math.random() - 0.5) * 10;
+    donut.position.z = (Math.random() - 0.5) * 10;
+    donut.rotation.x = Math.random() * Math.PI;
+    donut.rotation.y = Math.random() * Math.PI;
+    donut.rotation.z = Math.random() * Math.PI;
 
-// MeshNormalMaterial
-// const material = new THREE.MeshNormalMaterial()
-// material.side = THREE.DoubleSide // 오브젝트 앞뒷면 둘다 보이게
-// material.flatShading = true // 둥근 부분을 평평하게 (각지게)
+    const scale = Math.random();
+    donut.scale.x = scale;
+    donut.scale.y = scale;
+    donut.scale.z = scale;
 
-// MeshMatcapMaterial: 색상의 명암을 나타내 줄 수 있음/빛이 있는 것마냥 (조명이 필요없음)
-// const material = new THREE.MeshMatcapMaterial()
-// material.matcap = matcapTexture 
+    scene.add(donut);
+  }
+});
 
-// MeshDepthMaterial: 카메라를 가까이하면 보이고, 멀리하면 안 보임
-// const material = new THREE.MeshDepthMaterial()
+const axesHelper = new THREE.AxesHelper();
+// scene.add(axesHelper);
 
-// // MeshLambertMaterial: 조명이 필요함 
-// const material = new THREE.MeshLambertMaterial();
-// material.side = THREE.DoubleSide;
-
-// MeshPhongMaterial: 곡선 부분을 더 부드럽게
-// const material = new THREE.MeshPhongMaterial();
-// material.side = THREE.DoubleSide;
-// material.shininess = 100; // 반짝이는 정도
-// material.specular = new THREE.Color(0x1188ff) // 반짝이는 컬러
-
-// // MeshToonMaterial: 카툰 효과 (예: 젤다)
-// const material = new THREE.MeshToonMaterial();
-
-// MeshStandardMaterial: 옵션이 많아서 자주 쓰임
-// const material = new THREE.MeshStandardMaterial({
-//   metalness: 0.7,
-//   roughness: 0.2,
-//   map: doorColorTexture,
-//   aoMap: doorAmbientOcclusionTexture,
-//   aoMapIntensity: 1,
-//   displacementMap: doorHeightTexture,
-//   wireframe: false,
-//   displacementScale: 0.1
-  
-// });
-
-// material.metalnessMap = doorMetalnessTexture;
-// material.roughnessMap = doorRoughnessTexture;
-// material.normalMap = doorNormalTexture // 빛에 반응
-
-const material = new THREE.MeshPhysicalMaterial();
-  material.metalness =  0;
-  material.roughness =  0;
-//   material.map =  doorColorTexture;
-//   material.aoMap =  doorAmbientOcclusionTexture;
-//   material.aoMapIntensity =  1;
-//   material.displacementMap =  doorHeightTexture;
-//   material.wireframe =  false;
-//   material.displacementScale = 0.1
-// // material.alphaMap = doorAlphaColorTexture;
-//   material.transparent = true
-
-
-//   // Clearcoat 
-// material.clearcoat = 1;
-// material.clearcoatRoughness = 0
-material.ior = 1.5
-material.thickness = 0.5
-material.transmission = 1
-
-
-
-gui.add(material, "metalness").min(0).max(1).step(0.0001);
-gui.add(material, "roughness").min(0).max(1).step(0.0001);
-gui.add(material, "clearcoat").min(0).max(1).step(0.0001);
-gui.add(material, "clearcoatRoughness").min(0).max(1).step(0.0001);
-
-
-material.side = THREE.DoubleSide;
-
-/* Light */
-// const ambientLight = new THREE.AmbientLight(0xffffff, 1)
-// const pointLight = new THREE.PointLight(0xffffff, 30)
-// pointLight.position.x = 2;
-// pointLight.position.y = 3;
-// pointLight.position.z = 4;
-
-// scene.add(ambientLight)
-// scene.add(pointLight)
-
-/* background map loader */
-const rgbeLoader = new RGBELoader();
-rgbeLoader.load("/textures/environmentMap/2k.hdr", (map) => {
-  map.mapping = THREE.EquirectangularReflectionMapping;
-  scene.background = map;
-  scene.environment = map
-})
-
-
-/* Object */
-
-const sphere = new THREE.Mesh(
-  new THREE.SphereGeometry(0.5, 16, 16),
-  material
-)
-
-const plane = new THREE.Mesh(
-  new THREE.PlaneGeometry(1, 1),
-  material
-)
-
-const torus = new THREE.Mesh(
-  new THREE.TorusGeometry(0.3, 0.2, 16, 32), material
-)
-
-sphere.position.x = 1.5;
-torus.position.x = -1.5;
-
-scene.add(sphere, plane, torus)
-
-
-// camera 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 3
-
-
+// camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 3;
 
 scene.add(camera);
-
-// axes helper
-const axesHelper = new THREE.AxesHelper(2); // 2: 선의 길이
-
-scene.add(axesHelper)
-
 
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -184,47 +95,32 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 let now, delta;
 let then = Date.now();
-const interval = 1000 / 60
+const interval = 1000 / 60;
 
 const clock = new THREE.Clock();
 
-
 // control
 const control = new OrbitControls(camera, canvas);
-control.enableDamping = true
+control.enableDamping = true;
 
 // requestAnimationFrame
 
-
 const tick = () => {
-
   //elapsed time: 경과시간
   const elapsedTime = clock.getElapsedTime();
 
-
-  requestAnimationFrame(tick)
+  requestAnimationFrame(tick);
   now = Date.now();
   delta = now - then;
   if (delta < interval) return;
   // 모델링의 애니메이션을 먼저 설정해 주고
-  // Update object
-  // sphere.rotation.y = 0.1 * elapsedTime
-  // plane.rotation.y = 0.1 * elapsedTime
-  // torus.rotation.y = 0.1 * elapsedTime
 
-  // sphere.rotation.x = -0.15 * elapsedTime
-  // plane.rotation.x = -0.15 * elapsedTime
-  // torus.rotation.x = -0.15 * elapsedTime
-
-  control.update()
+  control.update();
 
   // renderer.render()로 다시 랜더링 시켜 주기
-  renderer.render(scene, camera)
+  renderer.render(scene, camera);
 
   then = now - (delta % interval);
-
-}
-
-
+};
 
 tick();
